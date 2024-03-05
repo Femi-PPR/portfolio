690fc2c81a38077e88d16719cf088df4b0be50de
Femi Oladapo <femioladapo3@gmail.com>
2024-03-05 15:06:45 +0000

    added php script for head of html

---
+++ inc/head.php
@@ -0,0 +1,10 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script>document.querySelector("html").className = "js";</script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="shortcut icon" href="images/icons/favicon.ico">
+    <title>Femi's Portfolio</title>
+</head>
